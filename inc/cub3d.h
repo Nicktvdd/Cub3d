038faaa4f08bb9c2de90eabb8b416f19969e37c0
jestebanpelaez18 <juanes.pelaez18@gmail.com>
2023-09-20 14:53:30 +0300

    remove submodule

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -3,39 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 16:22:10 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/09/19 19:20:29 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/09/20 14:43:34 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-
-# include <stdlib.h>
-# include <stdio.h>
-# include <unistd.h>
 # include "MLX42/MLX42.h"
-#define WIDTH 256
-#define HEIGHT 256
-
-
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# define WIDTH 256
+# define HEIGHT 256
 
 typedef struct s_data
 {
-    int     fd;
-    
-}   t_data;
+	int	fd;
 
+}		t_data;
 
 /*Map tools*/
 void	init_map(t_data *game, int argc, char **argv);
 
 /*Error tools*/
 void	error_msg(char *msg);
-
 
 #endif
