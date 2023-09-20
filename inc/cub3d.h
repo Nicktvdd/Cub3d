ffc80962b729d54c0ccba71a13ef87e4b6e1db23
jestebanpelaez18 <juanes.pelaez18@gmail.com>
2023-09-20 15:15:46 +0300

    last change

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,13 +6,15 @@
 /*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 16:22:10 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/09/20 14:43:34 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/09/20 15:08:27 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "libft/libft.h"
+# include "get_next_line/get_next_line.h"
 # include "MLX42/MLX42.h"
 # include <stdio.h>
 # include <stdlib.h>
--- src/start_map.c
+++ src/start_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   start_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 18:57:50 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/09/19 19:19:01 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/09/20 15:14:14 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,16 @@ static int	check_file(int argc, char **argument)
 		return (0);
 	return (1);
 }
+
 void	init_map(t_data *data, int argc, char **argv)
 {
-    if (!check_file(argc, argv))
+	if (!check_file(argc, argv))
 		error_msg("Error, invalid file format");
-    data->fd = open(argv[1],O_RDONLY);
-	if(data->fd == -1)
+	data->fd = open(argv[1], O_RDONLY);
+	if (data->fd == -1)
 		error_msg("Error file descriptor");
-	if(!parse_map_info(data->fd))
+	if (!parse_map_info(data->fd))
 		error_msg("wrong map information");
-	if(!parse_map(data->fd))
+	if (!parse_map(data->fd))
 		error_msg("Invalid map");
 }
